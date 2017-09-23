ad40952ef501baa6976287b3daf5bd1a397768cc
psalvatore <tuc45398@temple.edu>
2017-09-23 18:43:48 -0400

    add calculated fields rank and win_percentage to Player model

--- app/Player.php
+++ app/Player.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    //
+    protected $table = 'players';
+    protected $appends = ['rank', 'win_percentage'];
+
+    public function getRankAttribute()
+    {
+        return $this->newQuery()->where('wins', '>=', $this->wins)->count();
+    }
+
+    public function getWinPercentageAttribute()
+    {
+        if($this->games_played == 0){
+            $win_percentage = 0;
+        } else {
+            $win_percentage = $this->wins / $this->games_played;
+        }
+        return $win_percentage;
+    }
 }
